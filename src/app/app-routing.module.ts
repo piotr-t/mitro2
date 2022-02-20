65d44e05b446e82d417a5e096ce65a1650e9c90e
piotr-t <troc.piotr@gmail.com>
2022-02-21 00:40:06 +0100

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { StartPageComponent } from './start-page/start-page.component';
 
@@ -8,11 +7,13 @@ import { StartPageComponent } from './start-page/start-page.component';
 const routes: Routes = [
   { path: 'Page', component: HomeComponent },
   { path: 'Home', component: StartPageComponent },
-  { path: '**', component: StartPageComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/Home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/Home', pathMatch: 'full'  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], // { relativeLinkResolution: 'legacy' } LocationStrategy, {useClass: HashLocationStrategy}
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+   // { relativeLinkResolution: 'legacy' } LocationStrategy, {useClass: HashLocationStrategy}
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
